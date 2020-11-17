461b6bcae7105d0ef2da147749d895c5ac5bc3ac
mkamal21 <71103370+mkamal21@users.noreply.github.com>
2020-11-16 18:57:07 -0600

    Study App data

---
+++ StudyApp/StudyApp/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Mohammed Kamal 
+ * Used Progammingisfun.com
+ * 11/3/2020
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace StudyAppWithData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(); 
+
+            WriteLine($"Thank you for studying!");
+            
+        }
+    }
+} 
---
+++ StudyApp/StudyApp/TestTerms.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Mohammed Kamal 
+ * Used Progammingisfun.com
+ * 11/3/2020
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
+
+namespace StudyAppWithData
+{
+    class TestTerms
+    {
+       
+        public int score = 0;
+        public int total = 15;
+
+        public void askQuestions()
+        {
+            
+            string termfile = "Terms.txt";
+
+            string[] terms = File.ReadAllLines(termfile);
+
+
+            string definitionsfile = "Definitions.txt";
+
+            string[] Definitions = File.ReadAllLines(definitionsfile);
+
+
+
+
+            Random random = new Random();
+            int randomindex;
+
+            string answer = "";
+
+                for (int i = 0; i < Definitions.Length; i++)
+                {
+
+                    Clear();
+                    WriteLine($"Write the term for: ");
+                    randomindex = random.Next(0, Definitions.Length);
+                    
+                    
+                    WriteLine(Definitions[randomindex]);
+
+                    answer = ReadLine();
+
+                
+
+                    if (answer.ToLower() == terms[randomindex])
+                    {
+                        ForegroundColor = ConsoleColor.Blue;
+                        WriteLine("Correct!");
+
+                        ResetColor();
+
+                        score++;
+                        ReadKey();
+                    }
+
+                    else
+                    {
+                        ForegroundColor = ConsoleColor.Red;
+                        WriteLine($"Nope! It's actually {terms[randomindex]}.");
+
+                        ResetColor();
+
+                        ReadKey();
+                    }
+                    Clear();
+                }
+            
+
+
+            
+        }
+       
+    }
+}
